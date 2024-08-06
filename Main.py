bf9bfc1febc469489eaf3a94d185239cd60bad6e
Moral355NG <160786784+Moral355NG@users.noreply.github.com>
2024-08-06 21:13:56 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,12 +13,20 @@ tx1 = random.randint(0,640)
 ty1 = random.randint(-1000,0)
 tx2 = random.randint(0,640)
 ty2 = random.randint(-1000,0)
+lx = random.randint(0,640)
+ly = random.randint(-1000,0)
 ex1 = random.randint(0,640)
 ey1 = random.randint(-1000,0)
 ex2 = random.randint(0,640)
 ey2 = random.randint(-1000,0)
 ex3 = random.randint(0,640)
 ey3 = random.randint(-1000,0)
+ex4 = random.randint(0,640)
+ey4 = random.randint(-1000,0)
+ex5 = random.randint(0,640)
+ey5 = random.randint(-1000,0)
+prx = random.randint(0,640)
+pry = random.randint(-1000,0)
 vel = 1
 screen = pygame.display.set_mode((x,y))
 run = True
@@ -29,10 +37,14 @@ keys = pygame.key.get_pressed()
 playersprite = pygame.image.load("Assets/Player.png")
 enemysprite1 = pygame.image.load("Assets/BF-109.png")
 enemysprite2 = pygame.image.load("Assets/Avro Lancaster.png")
-enemysprite3 = pygame.image.load("Assets/HE-111Z.png")
+enemysprite3 = pygame.image.load("Assets/Bomber.png")
+enemysprite4 = pygame.image.load("Assets/Warthog.png")
+enemysprite5 = pygame.image.load("Assets/Tomcat.png")
 cloudsprite1 = pygame.image.load("Assets/Cloud 1.png")
 cloudsprite2 = pygame.image.load("Assets/Cloud 2.png")
 treesprite = pygame.image.load("Assets/Tree.png")
+lakesprite = pygame.image.load("Assets/Lake.png")
+parachutesprite = pygame.image.load("Assets/Parachute.png")
 gamelogo = pygame.image.load("Assets/Plane Box Logo.png")
 space2start = pygame.image.load("Assets/Space to start.png")
 
@@ -53,6 +65,10 @@ while run:
       ey2 = random.randint(-1000,0)
       ex3 = random.randint(0,640)
       ey3 = random.randint(-1000,0)
+      ex4 = random.randint(0,640)
+      ey4 = random.randint(-1000,0)
+      ex5 = random.randint(0,640)
+      ey5 = random.randint(-1000,0)
       #renders menu
       screen.fill((0,100,200))
       screen.blit(gamelogo,(213,100))
@@ -64,12 +80,16 @@ while run:
         screen.fill((0,100,0))
 
         #renders sprites
+        screen.blit(lakesprite,(lx,ly))
         screen.blit(treesprite,(tx1,ty1))
         screen.blit(treesprite,(tx2,ty2))
+        screen.blit(parachutesprite,(prx,pry))
         player = screen.blit(playersprite,(px,py))
         enemy1 = screen.blit(enemysprite1,(ex1,ey1))
         enemy2 = screen.blit(enemysprite2,(ex2,ey2))
         enemy3 = screen.blit(enemysprite3,(ex3,ey3))
+        enemy4 = screen.blit(enemysprite4,(ex4,ey4))
+        enemy5 = screen.blit(enemysprite5,(ex5,ey5))
         screen.blit(cloudsprite1,(cx1,cy1))
         screen.blit(cloudsprite2,(cx2,cy2))
 
@@ -80,38 +100,58 @@ while run:
             px = 0
 
         #cloud movement
-        if cy1 == 720:
+        if cy1 > 720:
             cx1 = random.randint(0,640)
             cy1 = random.randint(-1000,0)
-        if cy2 == 720:
+        if cy2 > 720:
            cx2 = random.randint(0,640)
            cy2 = random.randint(-1000,0)
         cy1 += vel
         cy2 += vel
 
         #tree movement
-        if ty1 == 720:
+        if ty1 > 720:
             tx1 = random.randint(0,640)
             ty1 = random.randint(-1000,0)
-        if ty2 == 720:
+        if ty2 > 720:
             tx2 = random.randint(0,640)
             ty2 = random.randint(-1000,0)
         ty1 += vel
         ty2 += vel
 
+        #Lake movement
+        if ly > 720:
+            lx = random.randint(0,640)
+            ly = random.randint(-1000,0)
+        ly += vel
+
+        #parachute movement
+        if pry > 720:
+            prx = random.randint(0,640)
+            pry = random.randint(-1000,0)
+        pry += vel
+
         #enemy movement
-        if ey1 == 720:
+        if ey1 > 720:
             ex1 = random.randint(0,640)
             ey1 = random.randint(-1000,0)
-        if ey2 == 720:
+        if ey2 > 720:
             ex2 = random.randint(0,640)
             ey2 = random.randint(-1000,0)
-        if ey3 == 720:
+        if ey3 > 720:
             ex3 = random.randint(0,640)
             ey3 = random.randint(-1000,0)
+        if ey4 > 720:
+            ex4 = random.randint(0,640)
+            ey4 = random.randint(-1000,0)
+        if ey5 > 720:
+            ex5 = random.randint(0,640)
+            ey5 = random.randint(-1000,0)
         ey1 += vel
         ey2 += vel
         ey3 += vel
+        ey4 += vel
+        ey5 += vel
 
         #collision
         if pygame.Rect.colliderect(player,enemy1):
@@ -119,6 +159,10 @@ while run:
         if pygame.Rect.colliderect(player,enemy2):
            status = "menu"
         if pygame.Rect.colliderect(player,enemy3):
+           status = "menu"
+        if pygame.Rect.colliderect(player,enemy4):
+           status = "menu"
+        if pygame.Rect.colliderect(player,enemy5):
            status = "menu"
 
         #keybinds
