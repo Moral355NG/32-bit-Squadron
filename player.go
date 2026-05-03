e7ae76826533cf3bf8bdab5ae6068e72fdb06eb5
Moral355NG <160786784+Moral355NG@users.noreply.github.com>
2026-05-03 18:07:23 +1000

    Refactor player movement and animation logic

--- player.go
+++ player.go
@@ -3,25 +3,33 @@ package main
 import "github.com/hajimehoshi/ebiten/v2"
 
 var (
-	playerSheetX    int
-	playerSheetY    int
-	playerAnimation int
+	playerSheetX   int
+	playerSheetY   int
+	playerState    int
+	playerWidth    float64
+	PlayerHeight   float64
+	playerPosition float64
 )
 
+func playerInit() {
+	playerPosition = 1280 / 2
+	playerWidth, PlayerHeight = 64, 64
+}
+
 func playerMovement() {
-	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) && playerPosition > 0 {
+	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) && playerPosition > 0+playerWidth/2 {
 		playerPosition -= 10
-		playerAnimation = -1
-	} else if ebiten.IsKeyPressed(ebiten.KeyArrowRight) && playerPosition < 1280-64 {
+		playerState = -1
+	} else if ebiten.IsKeyPressed(ebiten.KeyArrowRight) && playerPosition < 1280-playerWidth/2 {
 		playerPosition += 10
-		playerAnimation = 1
+		playerState = 1
 	} else {
-		playerAnimation = 0
+		playerState = 0
 	}
 }
 
-func playerAnimate() {
-	switch playerAnimation {
+func playerAnimation() {
+	switch playerState {
 	case -1: // Left
 		playerSheetX, playerSheetY = 0, 0
 	case 0: // Centre
