d04f065aef9bb9f3f9d0fff35f2c59c39c033bf7
Moral355NG <160786784+Moral355NG@users.noreply.github.com>
2026-05-20 20:52:54 +1000

    Add enemy spawning
    Added enemy initialization and update methods for movement and animation.

--- enemies.go
+++ enemies.go
@@ -17,13 +17,14 @@ package main
 
 import (
 	"image"
+	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 const ()
 
-// WIP
+// WIP ADD COLLISIONS AND MULTIPLE ENEMY SPAWNING LATER
 type Enemy struct {
 	x, y   float64
 	sheetX int
@@ -31,9 +32,30 @@ type Enemy struct {
 	width  float64
 	height float64
 	vel    float64
-	plane  int
+	state  int
 	img    *ebiten.Image
 	op     *ebiten.DrawImageOptions
+}
+
+func (e *Enemy) init() {
+	e.x = float64(rand.Intn(1280))
+	e.y = float64(rand.Intn(32))
+	e.width = 64
+	e.height = 64
+	e.vel = 10
+}
+
+func (e *Enemy) update() {
+	// enemy movement
+	if e.y > float64(height*2) {
+		e.x = float64(rand.Intn(1280))
+		e.y = float64(rand.Intn(32))
+		e.state = rand.Intn(5)
+	} else {
+		e.y += e.vel * scaling
+	}
+	// enemy animation
+	e.sheetX, e.sheetY = int(spriteWidth)*e.state, animationIndex*int(spriteHeight)
 }
 
 func (e *Enemy) draw(screen *ebiten.Image) {
@@ -41,7 +63,7 @@ func (e *Enemy) draw(screen *ebiten.Image) {
 	// define player position and size on screen
 	e.op.GeoM.Translate(-e.width/2, -e.height)
 	e.op.GeoM.Scale(1*scaling, 1*scaling)
-	e.op.GeoM.Translate(e.x*transform, float64(height))
+	e.op.GeoM.Translate(e.x*transform, e.y)
 	// formatting for subimage process(x, y, x + width, y + height)
 	screen.DrawImage(enemies.SubImage(image.Rect(e.sheetX, e.sheetY, e.sheetX+int(spriteWidth), e.sheetY+int(spriteHeight))).(*ebiten.Image), e.op)
 }
