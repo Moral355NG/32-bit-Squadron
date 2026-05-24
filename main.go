8fc23f633e5cd143c0f314eedbe87a8955754d96
Moral355NG <160786784+Moral355NG@users.noreply.github.com>
2026-05-24 10:54:10 +1000

    Refactor enemy initialization and add player reset
    Updated enemy initialization and player reset logic.

--- main.go
+++ main.go
@@ -24,6 +24,8 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	// TO-DO: Add collisions
+	//"https://github.com/tducasse/ebiten-collisions"
 )
 
 const (
@@ -58,9 +60,8 @@ func init() {
 	player, _, err = ebitenutil.NewImageFromFile("assets/player.png")
 	enemies, _, err = ebitenutil.NewImageFromFile("assets/enemies.png")
 	p = &Player{}
-	p.init()
+	p.Init()
 	e = &Enemy{}
-	e.init()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -81,11 +82,12 @@ func (g *Game) Init() {
 	g.op = &ebiten.DrawImageOptions{}
 
 	g.enemies.enemies = make([]*Enemy, 100)
-	g.enemies.count = 10
+	g.enemies.count = 16
 
 	for i := range g.enemies.enemies {
 		x := float64(rand.Intn(1280))
-		y := float64(rand.Intn(32))
+		y := 0 - float64(rand.Intn(height))
+		state := rand.Intn(5)
 		width := 64.0
 		height := 64.0
 		vel := 10.0
@@ -94,9 +96,14 @@ func (g *Game) Init() {
 			y:      y,
 			width:  width,
 			height: height,
+			state:  state,
 			vel:    vel,
 		}
 	}
+}
+
+func (g *Game) Reset() {
+	p.Init()
 }
 
 func (g *Game) Update() error {
@@ -106,7 +113,7 @@ func (g *Game) Update() error {
 	g.tick++
 	animationIndex = (g.tick / 5) % frameCount
 	width, height = ebiten.WindowSize()
-	p.update()
+	p.Update()
 	g.enemies.Update()
 	return nil
 }
@@ -117,15 +124,20 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	screen.Fill(color.RGBA{0, 50, 0, 255})
 	ebitenutil.DebugPrint(screen, "v1.1.0-alpha.1")
-	p.draw(screen)
+	p.op = &ebiten.DrawImageOptions{}
+	// draw player
+	p.op.GeoM.Translate(-p.width/2, -p.height)
+	p.op.GeoM.Scale(1*scaling, 1*scaling)
+	p.op.GeoM.Translate(p.x*transform, p.y)
+	screen.DrawImage(player.SubImage(image.Rect(p.sheetX, p.sheetY, p.sheetX+int(spriteWidth), p.sheetY+int(spriteHeight))).(*ebiten.Image), p.op)
 	// draw enemies
 	for i := 0; i < g.enemies.count; i++ {
-		s := g.enemies.enemies[i]
+		e := g.enemies.enemies[i]
 		g.op.GeoM.Reset()
-		g.op.GeoM.Translate(-s.width/2, -s.height)
+		g.op.GeoM.Translate(-e.width/2, -e.height)
 		g.op.GeoM.Scale(1*scaling, 1*scaling)
-		g.op.GeoM.Translate(s.x*transform, s.y)
-		screen.DrawImage(enemies.SubImage(image.Rect(s.sheetX, s.sheetY, s.sheetX+int(spriteWidth), s.sheetY+int(spriteHeight))).(*ebiten.Image), g.op)
+		g.op.GeoM.Translate(e.x*transform, e.y)
+		screen.DrawImage(enemies.SubImage(image.Rect(e.sheetX, e.sheetY, e.sheetX+int(spriteWidth), e.sheetY+int(spriteHeight))).(*ebiten.Image), g.op)
 	}
 }
 
