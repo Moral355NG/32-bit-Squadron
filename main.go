102e150721980f0afc40c1f2951e0d37d16bc08e
Moral355NG <160786784+Moral355NG@users.noreply.github.com>
2026-07-16 14:27:14 +1000

    Clean up unused vars and fix drawing order
    Clean up unused variables
    Modify drawing order so debug text is only drawn once per frame

--- main.go
+++ main.go
@@ -41,6 +41,7 @@ const (
 
 var (
 	scene          int
+	biome          int
 	points         int
 	multiplier     float64
 	windowWidth    int
@@ -51,6 +52,8 @@ var (
 	enemies        *ebiten.Image
 	p              *Player
 	e              *Enemy
+	g              *Game
+	op             *ebiten.DrawImageOptions
 )
 
 func init() {
@@ -78,7 +81,6 @@ func init() {
 }
 
 type Game struct {
-	op          *ebiten.DrawImageOptions
 	tick        int
 	enemies     Enemies
 	initialised bool
@@ -96,9 +98,10 @@ func (g *Game) Init() {
 		g.initialised = true
 	}()
 
-	g.op = &ebiten.DrawImageOptions{}
+	op = &ebiten.DrawImageOptions{}
 
-	g.enemies.enemies = make([]*Enemy, 100)
+	// initialise enemies
+	g.enemies.enemies = make([]*Enemy, 2048)
 	g.enemies.count = 12 * windowWidth / baseWidth
 	for i := range g.enemies.enemies {
 		x := float64(rand.Intn(1280))
@@ -137,9 +140,33 @@ func (g *Game) Update() error {
 	windowWidth, windowHeight = ebiten.WindowSize()
 	windowWidth = windowWidth * baseHeight / windowHeight
 	g.enemies.count = 12 * windowWidth / baseWidth
-	p.Update()
 	g.enemies.Update()
+	p.Update()
 	return nil
+}
+
+func (g *Game) Reset() {
+	// reset player
+	points = 0
+	p.x = float64(windowWidth) / 2
+	p.y = float64(baseHeight)
+	p.width = 64
+	p.height = 64
+	p.vel = 10
+	// reset enemies
+	e.x = float64(rand.Intn(windowWidth))
+	e.y = 0 - float64(windowHeight) - float64(rand.Intn(windowHeight))
+	e.state = rand.Intn(5)
+	switch e.state {
+	case 1:
+		e.vel = 11
+	case 3:
+		e.vel = 16
+	case 4:
+		e.vel = 12
+	default:
+		e.vel = 10
+	}
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
@@ -148,22 +175,24 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		screen.Fill(color.RGBA{50, 50, 50, 255})
 	case gameWorld:
 		screen.Fill(color.RGBA{0, 50, 0, 255})
-		p.op = &ebiten.DrawImageOptions{}
-		// draw player
-		p.op.GeoM.Translate(-p.width/2, -p.height)
-		p.op.GeoM.Scale(1, 1)
-		p.op.GeoM.Translate(p.x, p.y)
-		screen.DrawImage(player.SubImage(image.Rect(p.sheetX, p.sheetY, p.sheetX+int(spriteWidth), p.sheetY+int(spriteHeight))).(*ebiten.Image), p.op)
+		// to-do: draw world
+		// to-do: draw clouds
 		// draw enemies
 		for i := range g.enemies.count {
 			e := g.enemies.enemies[i]
-			g.op.GeoM.Reset()
-			g.op.GeoM.Translate(-e.width/2, -e.height)
-			g.op.GeoM.Scale(1, 1)
-			g.op.GeoM.Translate(e.x, e.y)
-			screen.DrawImage(enemies.SubImage(image.Rect(e.sheetX, e.sheetY, e.sheetX+int(spriteWidth), e.sheetY+int(spriteHeight))).(*ebiten.Image), g.op)
-			ebitenutil.DebugPrint(screen, "32-bit Squadron v1.1.0-alpha.1 - In Game - "+fmt.Sprintf("Points: %d", points))
+			op.GeoM.Reset()
+			op.GeoM.Translate(-e.width/2, -e.height)
+			op.GeoM.Scale(1, 1)
+			op.GeoM.Translate(e.x, e.y)
+			screen.DrawImage(enemies.SubImage(image.Rect(e.sheetX, e.sheetY, e.sheetX+int(spriteWidth), e.sheetY+int(spriteHeight))).(*ebiten.Image), op)
 		}
+		// draw player
+		op.GeoM.Reset()
+		op.GeoM.Translate(-p.width/2, -p.height)
+		op.GeoM.Scale(1, 1)
+		op.GeoM.Translate(p.x, p.y)
+		screen.DrawImage(player.SubImage(image.Rect(p.sheetX, p.sheetY, p.sheetX+int(spriteWidth), p.sheetY+int(spriteHeight))).(*ebiten.Image), op)
+		ebitenutil.DebugPrint(screen, "32-bit Squadron v1.1.0-alpha.1 - In Game - "+fmt.Sprintf("Points: %d", points))
 	case gameOver:
 		screen.Fill(color.RGBA{0, 0, 0, 255})
 		ebitenutil.DebugPrint(screen, "32-bit Squadron v1.1.0-alpha.1 - Game Over (Press Space to Restart) - "+fmt.Sprintf("Points: %d", points))
